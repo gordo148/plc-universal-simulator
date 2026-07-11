761c9248bd5fdad40ac3087b8ad99d046a0ada67
Rui Pires <rui.m.marques.p@gmail.com>
2026-07-11 09:26:20 +0100

    Improve Siemens polling, automatic versioning and Tag Manager performance

--- core/version.py
+++ core/version.py
@@ -6,11 +6,11 @@ Regenerate this file with ``scripts/generate_version.py`` during a build.
 import sys
 
 APP_NAME = 'PLC Universal Simulator'
-APP_VERSION = '2.2.2+0.gb3c826a.dirty'
+APP_VERSION = '2.2.2+1.g0f92199'
 APP_RELEASE = 'Development'
-APP_GIT_COMMIT = 'b3c826a'
+APP_GIT_COMMIT = '0f92199'
 APP_GIT_BRANCH = 'main'
-APP_BUILD_DATE = '2026-07-10 20:10 UTC'
+APP_BUILD_DATE = '2026-07-10 23:33 UTC'
 BUILD_TYPE = 'Source development build'
 
 
