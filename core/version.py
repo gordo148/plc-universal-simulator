89cd58972750e5c592f26bf4a2540601c1e4645d
Rui Pires <rui.m.marques.p@gmail.com>
2026-07-17 09:55:53 +0100

    chore: bump version after build

--- core/version.py
+++ core/version.py
@@ -6,11 +6,11 @@ Regenerate this file with ``scripts/generate_version.py`` during a build.
 import sys
 
 APP_NAME = 'PLC Universal Simulator'
-APP_VERSION = '2.2.4'
-APP_RELEASE = 'Stable'
-APP_GIT_COMMIT = '1a017c2'
+APP_VERSION = '2.2.5-rc1+2.g370c8be'
+APP_RELEASE = 'Development'
+APP_GIT_COMMIT = '370c8be'
 APP_GIT_BRANCH = 'main'
-APP_BUILD_DATE = '2026-07-13 12:06 UTC'
+APP_BUILD_DATE = '2026-07-17 08:54 UTC'
 BUILD_TYPE = 'Source development build'
 
 
